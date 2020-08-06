cb9bbfccefff018fa72a490a80c4c71d4ccff458
aliyundata <2269982328@qq.com>
2020-08-06 11:29:26 +0800

    代码错误修改

--- hive_udf_demo/src/main/java/pers/hwj/udf/returnSum.java
+++ hive_udf_demo/src/main/java/pers/hwj/udf/returnSum.java
@@ -1,11 +1,13 @@
 package pers.hwj.udf;
 
+import org.apache.hadoop.hive.ql.exec.UDF;
+
 /**
  * @Author hwj
  * @Date 2020/8/6 11:07
  * @Desc: 获得两数字的和
  **/
-public class returnSum {
+public class returnSum extends UDF {
     public Double SumTwo(Double a,Double b){
         if(a==null){
             a=0.0;
