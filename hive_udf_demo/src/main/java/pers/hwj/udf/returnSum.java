f0db6da5eff7a6e313b3da9ad76d8512f20bad5a
aliyundata <2269982328@qq.com>
2020-08-06 11:11:30 +0800

    创建 hive 用户自定义函数UDF_两数字相加

---
+++ hive_udf_demo/src/main/java/pers/hwj/udf/returnSum.java
@@ -0,0 +1,17 @@
+package pers.hwj.udf;
+
+/**
+ * @Author hwj
+ * @Date 2020/8/6 11:07
+ * @Desc:
+ **/
+public class returnSum {
+    public Double SumTwo(Double a,Double b){
+        if(a==null){
+            a=0.0;
+        }
+        if(b==null)
+            b=0.0;
+        return a+b;
+    }
+}
